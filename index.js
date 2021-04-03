5fad3826a1bdd17320c094e319b9c725506e94a6
ndevare13 <ndevare13@gmail.com>
2021-04-04 00:39:51 +0530

    Initial commit with basic functionality to add a book to the library

---
+++ index.js
@@ -0,0 +1,82 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+const mainBody = document.querySelector('#main-body');
+const formBody = document.querySelector('#form-body');
+
+const container = document.querySelector('#container');
+const addButton = document.querySelector('#addButton');
+const addButtonDiv = document.querySelector('#addButtonDiv');
+const addBookButton = document.querySelector('#add-book');
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    console.log(book.isRead);
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('card');
+    bookDiv.innerHTML = `<div class="grid-items">
+        <h4><b>${book.title}</b></h4>
+        <p>${book.author}</p>
+        <p><i class="fas fa-book"></i> ${book.pages} pages</p>
+        <p>${ifReadOrNot(book.isRead)}</p>
+    </div>`;
+
+    addButtonDiv.before(bookDiv);
+}
+
+addButton.addEventListener('click', event => {
+    event.preventDefault();
+
+    mainBody.classList.add('hide-div');
+    formBody.classList.remove('hide-div');
+});
+
+addBookButton.addEventListener('click', event => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const auther = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isRead = getValueFromRadioButton();
+
+    const bookToAdd = new Book(title, auther, pages, isRead);
+    addBookToLibrary(bookToAdd);
+
+    formBody.classList.add('hide-div');
+    mainBody.classList.remove('hide-div');
+
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+});
+
+// const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+
+const bookRead = document.querySelector('#bookReadYes');
+const bookNotRead = document.getElementById('bookReadNo');
+
+function getValueFromRadioButton() {
+    let isRead;
+    if (bookRead.checked) {
+        isRead = bookRead.value;
+        bookRead.checked = false;
+    } else {
+        isRead = bookNotRead.value;
+        bookNotRead.checked = false;
+    }
+    return isRead;
+}
+
+function ifReadOrNot(isRead) {
+    if (isRead === 'true') {
+        return 'Read';
+    } else {
+        return 'Not yet read';
+    }
+}
