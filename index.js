7dc0682ce0727ae22a73fd252dae96285ed45333
ndevare13 <ndevare13@gmail.com>
2021-04-04 23:15:09 +0530

    Added functionality to mark a book as Read and made few changes in the code to make sure all form elements except Reading status are required fields

--- index.js
+++ index.js
@@ -9,28 +9,24 @@ function Book(title, author, pages, isRead) {
 
 const mainBody = document.querySelector('#main-body');
 const formBody = document.querySelector('#form-body');
+const formElement = document.querySelector('form');
 
 const container = document.querySelector('#container');
 const addButton = document.querySelector('#addButton');
 const addButtonDiv = document.querySelector('#addButtonDiv');
-const addBookButton = document.querySelector('#add-book');
 let removeButtons = document.querySelectorAll('.removeButton');
+let markBookAsReadButtons = document.querySelectorAll('.mark-read');
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
     const bookDiv = document.createElement('div');
     bookDiv.classList.add('card');
 
-    bookDiv.innerHTML = `<button class="removeButton"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="window-close" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-window-close fa-w-16 fa-3x"><path fill="currentColor" d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-83.6 290.5c4.8 4.8 4.8 12.6 0 17.4l-40.5 40.5c-4.8 4.8-12.6 4.8-17.4 0L256 313.3l-66.5 67.1c-4.8 4.8-12.6 4.8-17.4 0l-40.5-40.5c-4.8-4.8-4.8-12.6 0-17.4l67.1-66.5-67.1-66.5c-4.8-4.8-4.8-12.6 0-17.4l40.5-40.5c4.8-4.8 12.6-4.8 17.4 0l66.5 67.1 66.5-67.1c4.8-4.8 12.6-4.8 17.4 0l40.5 40.5c4.8 4.8 4.8 12.6 0 17.4L313.3 256l67.1 66.5z" class=""></path></svg></button>
-    <div class="grid-items">
-        <h4><b>${book.title}</b></h4>
-        <p>${book.author}</p>
-        <p><i class="fas fa-book"></i> ${book.pages} pages</p>
-        <p>${ifReadOrNot(book.isRead)}</p>
-    </div>`;
+    bookDiv.innerHTML = addHTMLCodeDependingOnReadOrNot(book);
 
     addButtonDiv.before(bookDiv);
 
+    markBookAsRead();
     removeBook();
 }
 
@@ -41,7 +37,7 @@ addButton.addEventListener('click', event => {
     mainBody.style.opacity = '10%';
 });
 
-addBookButton.addEventListener('click', event => {
+formElement.addEventListener('submit', event => {
     event.preventDefault();
 
     const title = document.querySelector('#title').value;
@@ -70,6 +66,18 @@ function removeBook() {
     });
 }
 
+function markBookAsRead() {
+    markBookAsReadButtons = document.querySelectorAll('.mark-read');
+
+    markBookAsReadButtons.forEach(markBookAsReadButton => {
+        markBookAsReadButton.addEventListener('click', event => {
+            event.preventDefault();
+            markBookAsReadButton.parentElement.querySelector('.reading-status').innerHTML = 'Read';
+            markBookAsReadButton.parentElement.removeChild(markBookAsReadButton);
+        });
+    });
+}
+
 const bookRead = document.querySelector('#bookReadYes');
 const bookNotRead = document.getElementById('bookReadNo');
 
@@ -85,10 +93,23 @@ function getValueFromRadioButton() {
     return isRead;
 }
 
-function ifReadOrNot(isRead) {
-    if (isRead === 'true') {
-        return 'Read';
+function addHTMLCodeDependingOnReadOrNot(book) {
+    if (book.isRead === 'true') {
+        return `<button class="removeButton"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="window-close" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-window-close fa-w-16 fa-3x"><path fill="currentColor" d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-83.6 290.5c4.8 4.8 4.8 12.6 0 17.4l-40.5 40.5c-4.8 4.8-12.6 4.8-17.4 0L256 313.3l-66.5 67.1c-4.8 4.8-12.6 4.8-17.4 0l-40.5-40.5c-4.8-4.8-4.8-12.6 0-17.4l67.1-66.5-67.1-66.5c-4.8-4.8-4.8-12.6 0-17.4l40.5-40.5c4.8-4.8 12.6-4.8 17.4 0l66.5 67.1 66.5-67.1c4.8-4.8 12.6-4.8 17.4 0l40.5 40.5c4.8 4.8 4.8 12.6 0 17.4L313.3 256l67.1 66.5z" class=""></path></svg></button>
+        <div class="grid-items">
+            <h4><b>${book.title}</b></h4>
+            <p>${book.author}</p>
+            <p><i class="fas fa-book"></i> ${book.pages} pages</p>
+            <p class="reading-status">Read</p>
+        </div>`;
     } else {
-        return 'Not yet read';
+        return `<button class="removeButton"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="window-close" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-window-close fa-w-16 fa-3x"><path fill="currentColor" d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-83.6 290.5c4.8 4.8 4.8 12.6 0 17.4l-40.5 40.5c-4.8 4.8-12.6 4.8-17.4 0L256 313.3l-66.5 67.1c-4.8 4.8-12.6 4.8-17.4 0l-40.5-40.5c-4.8-4.8-4.8-12.6 0-17.4l67.1-66.5-67.1-66.5c-4.8-4.8-4.8-12.6 0-17.4l40.5-40.5c4.8-4.8 12.6-4.8 17.4 0l66.5 67.1 66.5-67.1c4.8-4.8 12.6-4.8 17.4 0l40.5 40.5c4.8 4.8 4.8 12.6 0 17.4L313.3 256l67.1 66.5z" class=""></path></svg></button>
+        <div class="grid-items">
+            <h4><b>${book.title}</b></h4>
+            <p>${book.author}</p>
+            <p><i class="fas fa-book"></i> ${book.pages} pages</p>
+            <p class="reading-status">Not yet read</p>
+            <button class="mark-read">Mark this book as Read</button>
+        </div>`;
     }
 }
